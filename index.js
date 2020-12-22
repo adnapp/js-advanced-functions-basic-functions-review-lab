068a910b84a8a8d3fcb5e43739884c38fdc0576b
adnapp <adam.napps@gmail.com>
2020-12-22 14:30:24 -0500

    commit tree d87e0baa3dc5f341d386077b38c0ae19a03c9235

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+function saturdayFun(event = "roller-skate"){
+    return `This Saturday, I want to ${event}!`
+}
+
+function mondayWork(event = "go to the office"){
+    return `This Monday, I will ${event}.`
+}
+
+function wrapAdjective(flair = '*') {
+    return function(adj = "special"){
+        return `You are ${flair}${adj}${flair}!` 
+    }
+}
+
+let Calculator ={
+    add: function(a,b){
+        return a+b
+    },
+    subtract: function(a,b){
+        return a-b
+    },
+    multiply: function(a,b){
+        return a*b
+    },
+    divide: function(a,b){
+        return a/b
+    }
+}
+
+function actionApplyer(start,arr){
+    let a=start 
+
+    for (let i = 0; i < arr.length; i++ ){
+        a = arr[i](a)
+      }
+    
+      return a
+}
+
